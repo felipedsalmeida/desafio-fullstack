269793325f5135f7e8c3cf1f3c9232c77c68f227
Felipe Almeida <felipe_almeidarj@msn.com>
2023-06-05 01:27:49 -0300

    fixed app.ts

--- src/services/contacts/listContacts.service.ts
+++ src/services/contacts/listContacts.service.ts
@@ -24,7 +24,7 @@ const listContactsService = async (clientId:string): Promise<TContactsResponse> 
             client: client
         }
     })
-    console.log(contacts)
+    
     return contactsSchemaResponse.parse(contacts)
 }
 
